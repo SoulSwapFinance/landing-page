995502ca27e68fdf52a172cf15dd81f6751bbf63
Buns Enchantress <bunsthedev@gmail.com>
2023-08-03 17:21:12 -0400

    update: header, logo

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,6 @@
-// import WalletConnect from "components/WalletConnect";
+import WalletConnect from "components/WalletConnect";
 import Link from "next/link"
+import Image from "next/image"
 import { Fragment, useState } from "react"
 
 const Header = () => {
@@ -7,7 +8,6 @@ const Header = () => {
   // const [homeToggle, setHomeToggle] = useState(false);
   return (
     <Fragment>
-      {/* Right Navigation */}
       <div
         className={`nav_overlay ${toggle ? "go" : ""}`}
         onClick={() => setToggle(false)}
@@ -15,7 +15,6 @@ const Header = () => {
       <div className={`soul_fn_nav ${toggle ? "go" : ""}`}>
         <div className="trigger is-active">
           <div className="trigger_in" onClick={() => setToggle(false)}>
-            {/* <span className="text">Close</span> */}
             <span className="hamb">
               <span className="hamb_a" />
               <span className="hamb_b" />
@@ -24,204 +23,110 @@ const Header = () => {
           </div>
         </div>
         <div className="nav_content">
-          <div className="nav_menu">
-            <ul>
-              {/* <li className="menu-item">
-                  <WalletConnect />
-                </li> */}
-              <li className="menu-item">
-                <Link href="/">
-                <a>
-                  <img 
-                    src={'/img/text/home.png'}
-                    height={36}
-                    width={164}
-                    alt="home"
-                  />
-                </a>
-                </Link>
-              </li>
-              <li className="menu-item">
-                <Link 
-                  href="https://exchange.soulswap.finance"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img 
-                    src={'/img/text/exchange.png'}
-                    height={36}
-                    width={164}
-                    alt="exchange"
-                  />
-                </a>
-                </Link>
-              </li>
-              <li className="menu-item">
-                <Link 
-                  href="https://exchange.soulswap.finance/farm"
-                >
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img 
-                    src={'/img/text/summon.png'}
-                    height={36}
-                    width={164}
-                    alt="summon"
-                  />
-                </a>
-                </Link>
-              </li>
-              <li className="menu-item">
-                <Link 
-                  href="https://exchange.soulswap.finance/defarms"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img 
-                    src={'/img/text/launch.png'}
-                    height={36}
-                    width={164}
-                    alt="launch"
-                  />
-                </a>
-                </Link>
-              </li>
-              {/* <li className="menu-item">
-                <Link href="/Mint">
-                <a>
-                  <img
-                    src={'/img/text/Mint.png'}
-                    height={36}
-                    width={164}
-                    alt="mint"
-                  />
-                </a>
-                </Link>
-              </li> */}
-              {/* <li className="menu-item">
-                <Link href="/about">
-                <a>
-                  <img 
-                    src={'/img/text/about.png'}
-                    height={36}
-                    width={164}
-                    alt="about"
-                  />
-                </a>
-                </Link>
-              </li> */}
-              {/* <li className="menu-item">
-                <Link href="/brujas">
-                <a>
-                  <img 
-                    src={'/img/text/brujas.png'}
-                    height={48}
-                    width={164}
-                    alt="brujas"
-                  />
-                </a>
-                </Link>
-              </li>
-              <li className="menu-item">
-                <Link href="/Roadmap">
-                <a>
-                  <img 
-                    src={'/img/text/Roadmap.png'}
-                    height={36}
-                    width={164}
-                    alt="roadmap"
-                  />
-                </a>
-                </Link>
-              </li> */}
-            </ul>
-          </div>
-          {/* <div className="nav_buttons">
-            <a
-              href="https://opensea.io/"
-              className="soul_fn_button"
-              target="_blank"
-            >
-              <span className="icon">
-                <img src="svg/opensea.svg" alt="" className="fn__svg" />
-              </span>
-              <span className="text">OpenSea</span>
-            </a>
-          </div> */}
         </div>
         {/* Nav Footer */}
         <div className={`nav_footer ${toggle ? "ready" : ""}`}>
-          {/* <div className="nf_left">
-            <p
-              className={'font-bold'}
-            >
-             Developed by{" "}
-              <a
-                href="https://twitter.com/0xBuns"
-                target="_blank"
-              >
-                Buns Enchantress
-              </a>
-            </p>
-          </div> */}
-          {/* <div className="nf_right">
-            <div className="soul_fn_social_list">
-              <ul>
-                <li>
-                  <a href="#">
-                    <i className="fn-icon-twitter" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i className="fn-icon-instagram" />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div> */}
+          
         </div>
-        {/* !Nav Footer */}
       </div>
-      {/* !Right Navigation */}
-      {/* Header */}
       <header className="soul_fn_header">
         <div className="container">
           <div className="header_in">
-          <div className="logo">
-              <Link href="/">
-                <a>
-                  <img 
-                    src="/favicon.png"
-                    alt="bruja text logo"
-                    height={48}
-                    
-                  />
-                </a>
+            <div
+              style={{
+                display: 'flex', 
+                gap: '12px', 
+                flexDirection: 'row', 
+                justifyContent: 'space-between', 
+                alignItems: 'center'
+              }}
+            >
+              <Link
+                href="/"
+              >
+             <Image
+                src="/logo.png"
+                height={48}
+                width={48}
+                alt="soul logo"
+             />
+             </Link>
+            </div>
+            {/* <WalletConnect /> */}
+            <div
+              style={{
+                display: 'flex', 
+                gap: '12px', 
+                flexDirection: 'row', 
+                justifyContent: 'space-between', 
+                alignItems: 'center'
+              }}
+            >
+              <Link
+                href="https://discord.gg/soulswap"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    border: '1px solid #fff',
+                    padding: '0 12px',
+                    borderRadius: '12px',
+                    borderColor: '#af81f7'
+                  }}
+                >
+                  {'Discord'}
+                </div>
               </Link>
-            </div>
-            
-      {/* <WalletConnect /> */}
-            <div className="trigger">
-              <div className="trigger_in" onClick={() => setToggle(!toggle)}>
-                {/* <span className="text">Menu</span> */}
-                <span className="hamb">
-                  <span className="hamb_a" />
-                  <span className="hamb_b" />
-                  <span className="hamb_c" />
-                </span>
-              </div>
-            </div>
+              <Link
+                href="https://twitter.com/SoulSwapFinance"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    border: '1px solid #fff',
+                    padding: '0 12px',
+                    borderRadius: '12px',
+                    borderColor: '#af81f7'
+                  }}
+                >
+                  {'Twitter'}
+                </div>
+              </Link>
+              {/* <Link
+                href="https://docs.soulswap.finance"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    border: '1px solid #fff',
+                    padding: '0 12px',
+                    borderRadius: '12px',
+                    borderColor: '#af81f7'
+                  }}
+                >
+                  {'Docs'}
+                </div>
+              </Link> */}
+              <Link
+                href="https://links.soulswap.finance"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div
+                  style={{
+                    border: '1px solid #fff',
+                    padding: '0 12px',
+                    borderRadius: '12px',
+                    borderColor: '#af81f7'
+                  }}
+                >
+                  {'Links'}
+                </div>
+              </Link>
+          </div>
           </div>
         </div>
       </header>
